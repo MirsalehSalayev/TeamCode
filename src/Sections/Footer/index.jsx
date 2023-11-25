8125a073e04249bca78228ef7821bc412754e949
rufatsoltanli <mi7a53q2b@code.edu.az>
2023-11-25 18:20:51 +0400

    footer_structure_change

--- src/Sections/Footer/index.jsx
+++ src/Sections/Footer/index.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import Card from '../../Components/Card'
+import "./index.scss"
 
-const Footer = () => {
+function Footer() {
     return (
-        <Card />
+        <footer>
+            <div id="footerNav">
+                <div className="leftSide">
+                    <div className="footerBookLandImg">
+                        <img src="https://bookland.wprdx.com/demo/wp-content/themes/bookland/assets/images/logo-white.png" alt="" />
+                    </div>
+                    <div className="footerBookLandText">
+                        Bookland is a Book Store Ecommerce Website Template by DexignZone lorem ipsum dolor sit
+                    </div>
+                    <div className="footerBookLandIcons">
+                        <div className="icon"><i class="fa-brands fa-facebook-f"></i></div>
+                        <div className="icon"><i class="fa-brands fa-twitter"></i></div>
+                        <div className="icon"><i class="fa-brands fa-linkedin"></i></div>
+                        <div className="icon"><i class="fa-brands fa-instagram"></i></div>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
     )
 }
 
