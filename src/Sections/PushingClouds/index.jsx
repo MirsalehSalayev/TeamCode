d974f00237bf5b5236796ec59b4be3791ca53fae
MirsalehSalayev <saalayevsaleh@gmail.com>
2023-11-26 12:31:53 +0400

    pushing

--- src/Sections/PushingClouds/index.jsx
+++ src/Sections/PushingClouds/index.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
+import "./_pushing.scss"
 import Button from '../../Components/Button'
 
 
 const Pushing = () => {
     return (
-        <div>
-            <div>
-                <div>
+        <div className='pushingComp'>
+            <div className='pushingContOne'>
+                <div className='bestCont'>
                     <p>BEST SELLER</p>
                     <h2>Pushing Clouds</h2>
-                    <div>
+                    <div className='NapoleonCont'>
                         <p>Napoleon Hill</p>
                         <p>Sports</p>
                     </div>
                 </div>
-                <div>
+                <div className='titleCont'>
                     <p>
                         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal.
                     </p>
-                    <div>
+                    <div className='priceCont'>
                         <p>$15.21</p>
                         <p>$17.2</p>
                         <p>6% off</p>
                     </div>
-                    <div>
-                        <Button />
-                        <Button />
+                    <div className='buttonCont'>
+                        <Button title='Buy Now' />
+                        <Button title='Buy Now' />
                     </div>
                 </div>
             </div>
-            <div></div>
+            <div>
+
+            </div>
 
         </div>
     )
